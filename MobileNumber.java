888969bbba91aa21d3cc9969165099cf98891667
Kumar D <kumar.dodda@gmail.com>
2021-07-01 09:25:47 +0530

    Java 8

---
+++ MobileNumber.java
@@ -0,0 +1,17 @@
+package java8_Stream.interview;
+
+class MobileNumber{
+    private String number;
+ 
+    public MobileNumber(String number) {
+        this.number = number;
+    }
+ 
+    public String getNumber() {
+        return number;
+    }
+ 
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
